e6c443d6201796c0112dc4f49fd9ea290a87c188
pichi <pichi3009@gmail.com>
2023-02-05 19:03:30 -0300

    Carousel component - features dev: button and touch

--- src/components/CardSelector.jsx
+++ src/components/CardSelector.jsx
@@ -10,39 +10,45 @@ export function CardSelector() {
         ".asperiores illo ullam et incidunt ab non fugiat voluptates quasi tenetur nihil reprehenderit repellendus, " +
         "excepturi fugit sunt facere dolor doloribus? Placeat minima ex voluptates.";
 
-    const datos = [{imgSrc:'src/assets/zeus.jpg', title:"prueba 1", header: "29 enero - 2023",
-        text: text},{imgSrc:'src/assets/mano.jpg', title:"prueba 2", header: "29 enero - 2023",
-        text: text}];
+    const datos = [{
+        imgSrc: 'src/assets/zeus.jpg', title: "prueba 1", header: "29 enero - 2023",
+        text: text
+    }, {
+        imgSrc: 'src/assets/mano.jpg', title: "prueba 2", header: "29 enero - 2023",
+        text: text
+    }];
 
     return (
         <>
-            <section>
-                <Carousel>
-                    <CarouselItem>
-                        <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
-                                     text={"Description of project. Lorem ipsum dolor sit amet " +
-                                         "consectetur  .asperiores illo ullam et incidunt ab non fugiat voluptates " +
-                                         "quasi tenetur nihil reprehenderit repellendus, excepturi fugit sunt " +
-                                         "facere dolor doloribus? Placeat minima ex voluptates."}/>
-                    </CarouselItem>
-                    <CarouselItem>
-                        <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
-                                     text={"hola2"}/>
-                    </CarouselItem>
-                    <CarouselItem>
-                        <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
-                                     text={"hola3"}/>
-                    </CarouselItem>
-                    <CarouselItem>
-                        <h1>Hola1</h1>
-                    </CarouselItem>
-                    <CarouselItem>
-                        <h1>Hola2</h1>
-                    </CarouselItem>
-                    <CarouselItem>
-                        <h1>Hola3</h1>
-                    </CarouselItem>
-                </Carousel>
+            <section className={"prueba2"}>
+                <div className={"prueba1"}>
+                    <Carousel>
+                        <CarouselItem>
+                            <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
+                                         text={"Description of project. Lorem ipsum dolor sit amet " +
+                                             "consectetur  .asperiores illo ullam et incidunt ab non fugiat voluptates " +
+                                             "quasi tenetur nihil reprehenderit repellendus, excepturi fugit sunt " +
+                                             "facere dolor doloribus? Placeat minima ex voluptates."}/>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
+                                         text={"hola2"}/>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
+                                         text={"hola3"}/>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <h1>Hola1</h1>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <h1>Hola2</h1>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <h1>Hola3</h1>
+                        </CarouselItem>
+                    </Carousel>
+                </div>
             </section>
             <section>
                 <CardProject imgSrc={'src/assets/zeus.jpg'} title={"cv digital"} header={"29 enero - 2023"}
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import {useEffect, useState, useRef, Children} from "react";
 import {CardProject} from "../CardProject.jsx";
+import {useSwipeable} from "react-swipeable";
+
 
 export function CarouselItem({children}) {
     return (
@@ -12,30 +14,69 @@ export function CarouselItem({children}) {
 
 
 export default function Carousel({children}) {
+    const carouselRef = useRef();
     const [index, setIndex] = useState(0)
+    const [startX, setStartX] = useState(0);
+    const [offset, setOffset] = useState(0);
+    const [width, setWidth] = useState(0);
+    const [isDragging, setIsDragging] = useState(false);
+
+    useEffect(() => {
+        setWidth(carouselRef.current.offsetWidth)
+    }, []);
+
+
     let nextItem = (delta) => {
 
-        if (index + delta > React.Children.count(children) - 1) { //
+        if (index + delta > Children.count(children) - 1) { //
             setIndex(0);
         } else if (index + delta < 0) {
-            setIndex(React.Children.count(children) - 1); //
+            setIndex(Children.count(children) - 1); //
         } else {
             setIndex(index + delta)
         }
     }
 
+    let printStart = (e) => {
+        setIsDragging(true)
+        setStartX(e.touches[0].clientX);
+    }
 
+    let printEnd = (e) => {
+        setIsDragging(false)
+        const endX = e.changedTouches[0].clientX
+        // console.log(endX-startX)
+        if (endX - startX <  -(width / 3)) {
+            nextItem(1)
+        } else if (endX - startX  > (width / 3)) {
+            nextItem(-1)
+        }
+        setOffset(0)
+
+    }
+    let printMove = (e) => {
+        if (!isDragging) return;
+
+        let moveOffset = (100*(e.changedTouches[0].clientX - startX))/width
+        setOffset(moveOffset);
+    }
+
+    // style={{transform: `translateX(${translateValue - index * width}px)`}}
     return (
         <>
-            <div className="carousel">
-                <div className="carousel-item-container" style={{transform: `translateX(-${index * 100}%)`}}>
+            <div className="carousel" onTouchStart={printStart} onTouchEnd={printEnd} onTouchMove={printMove}   onMouseDown={printStart}
+                 onMouseMove={printMove}
+                 onMouseUp={printEnd}>
+                <div className="carousel-item-container" ref={carouselRef}
+                     style={{transform: `translateX(-${index * 100 - offset}%)`}}
+
+                >
                     {children}
                 </div>
                 <div className={"carousel-btn left-btn"}
                      onClick={() => {
                          nextItem(-1)
                      }}/>
-
                 <div className={"carousel-btn right-btn"}
                      onClick={() => {
                          nextItem(1)
